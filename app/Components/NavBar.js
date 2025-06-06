f078c505faa51523104ad46930d31c7dc39b6cbb
Manigandan13 <138948807+Manigandan13@users.noreply.github.com>
2025-06-06 16:05:16 +0530

    Update NavBar.js

--- app/Components/NavBar.js
+++ app/Components/NavBar.js
@@ -9,7 +9,7 @@ const NavBar = () => {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <nav className="bg-[#2f2626] fixed shadow-lg">
+    <nav className="bg-[#2f2626] shadow-lg">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16 items-center">
           <div className="flex-shrink-0">
