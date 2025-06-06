00255961b8f9d1752c8081618d7b62f2f6ad86be
Manigandan13 <138948807+Manigandan13@users.noreply.github.com>
2025-06-06 12:40:45 +0530

    Update HomePage.js

--- app/Components/HomePage.js
+++ app/Components/HomePage.js
@@ -8,7 +8,7 @@ const HomePage = () => {
           <div className="text-center text-white px-6">
               <h1 className="text-5xl font-semibold">Welcome to SSS College</h1>
               <p className="text-lg mt-4">Empowering the future with quality education</p>
-              <Link href="/enroll" className="mt-6 inline-block bg-[hsl(275,88%,45%)] text-white py-2 px-6 rounded-lg hover:bg-[hsl(275,88%,55%)]">
+              <Link href="/apply" className="mt-6 inline-block bg-[hsl(275,88%,45%)] text-white py-2 px-6 rounded-lg hover:bg-[hsl(275,88%,55%)]">
               Enroll Now
               </Link>
          </div>
